fd9cf58d387864a5edfb8ffcfedcf806dd6ec16c
Unknown <vlad.potapov1997@gmail.com>
2018-03-14 17:55:15 +0300

    All Css methods was replaced by Xpath methods

--- main.rb
+++ main.rb
@@ -21,7 +21,7 @@ end
 def get_goods(pages_array, file_name)
   doc = Nokogiri::HTML(pages_array.body)
   threads = []
-  doc.xpath('//*[contains(@class ,"product_img_link")]').to_a.each_with_index do |link, index|
+  doc.xpath('//a[contains(@class ,"product_img_link")]').to_a.each_with_index do |link, index|
     threads << Thread.new do
       get_info(Curl.get(link.attribute('href').value), file_name)
     end
@@ -32,17 +32,33 @@ end
 def get_info(good_page, file_name)
   good_array = []
   doc = Nokogiri::HTML(good_page.body)
-  good_name = doc.css('.nombre_producto').first.inner_text.strip
-  weight_array = doc.css('span.attribute_name').to_a
-  price_array = doc.css('span.attribute_price').to_a
+  good_name = doc.xpath('//h1[contains(@class ,"nombre_producto")]').first.inner_text.strip
+  weight_array = doc.xpath('//span[contains(@class, "attribute_name")]').to_a
+  price_array = doc.xpath('//span[contains(@class, "attribute_price")]').to_a
   unless weight_array.empty?
+    images_array = get_images(doc, good_page.url)
     weight_array.each_with_index do |weight, index|
-      good_array.push([good_name + ' - ' + weight.inner_text.strip, price_array[index].inner_text.strip, doc.css('#view_full_size > img').first.attribute('src').value])
+      good_array.push([good_name + ' - ' + weight.inner_text.strip, price_array[index].inner_text.strip, images_array[index]])
     end
   else
-    good_array.push([good_name, doc.css('span#our_price_display').first.inner_text, doc.css('#view_full_size > img').first.attribute('src').value])
+    good_array.push([good_name, doc.xpath('//span[contains(@id, "our_price_display")]').first.inner_text, doc.xpath('//span[contains(@id, "view_full_size")]/img').first.attribute('src').value])
   end
   add_to_csv(good_array, file_name)
+end
+
+def get_images(doc, url)
+  arr = []
+  doc.xpath('//script').each do |script|
+    arr.push(script.to_s)
+  end
+  images_array = []
+  arr[0].match(/attributesCombinations = (.*);/).to_s.scan(/"attribute":"(.*?)",.*?"group":"(.*?)"/).each do |match|
+    page = Curl.get(url + '#/' + (match[1] + '-' + match[0]))
+    # sleep(0.5)
+    doc = Nokogiri::HTML(page.body)
+    images_array.push(doc.xpath('//span[contains(@id, "view_full_size")]/img').first.attribute('src').value)
+  end
+  return images_array
 end
 
 def add_to_csv(array, file_name)
